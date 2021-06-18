1767db07fd6e7e46dace507ec852b5137e9fff69
Luannet <43404582+Luannet@users.noreply.github.com>
2021-06-18 22:24:58 +0200

    Update circuit

--- storage-proofs-post/src/fallback/circuit.rs
+++ storage-proofs-post/src/fallback/circuit.rs
@@ -167,89 +167,36 @@ impl<Tree: 'static + MerkleTreeTrait> Circuit<Bls12> for &Sector<Tree> {
         } 
 
         let len = leafs.len();
-        let mut gen_cs = cs.make_vector_copy(len)?;
-        let unit = cs.make_copy()?;
+        let algo_type = get_algorithm_type();
         // 2. Verify Inclusion Paths
-        for (i, (leaf, path)) in leafs.iter().zip(paths.iter()).enumerate() {
-            PoRCircuit::<Tree>::synthesize(
-                cs.namespace(|| format!("challenge_inclusion_{}", i)),
-                Root::Val(*leaf),
-                path.clone(),
-                Root::from_allocated::<CS>(comm_r_last_num.clone()),
-                true,
-            )?;
+        if algo_type == 1 {
+            for (i, (leaf, path)) in leafs.iter().zip(paths.iter()).enumerate() {
+                PoRCircuit::<Tree>::synthesize(
+                    cs.namespace(|| format!("challenge_inclusion_{}", i)),
+                    Root::Val(*leaf),
+                    path.clone(),
+                    Root::from_allocated::<CS>(comm_r_last_num.clone()),
+                    true,
+                )?;
+            }
         }
-
-        Ok(())
-    }
-
-    fn custom_synthesize<CS: ConstraintSystem<Bls12>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
-        let Sector {
-            comm_r,
-            comm_c,
-            comm_r_last,
-            leafs,
-            paths,
-            ..
-        } = self;
-
-        assert_eq!(paths.len(), leafs.len());
-
-        // 1. Verify comm_r
-        let comm_r_last_num = AllocatedNum::alloc(cs.namespace(|| "comm_r_last"), || {
-            comm_r_last
-                .map(Into::into)
-                .ok_or(SynthesisError::AssignmentMissing)
-        })?;
-
-        let comm_c_num = AllocatedNum::alloc(cs.namespace(|| "comm_c"), || {
-            comm_c
-                .map(Into::into)
-                .ok_or(SynthesisError::AssignmentMissing)
-        })?;
-
-        let comm_r_num = AllocatedNum::alloc(cs.namespace(|| "comm_r"), || {
-            comm_r
-                .map(Into::into)
-                .ok_or(SynthesisError::AssignmentMissing)
-        })?;
-
-        comm_r_num.inputize(cs.namespace(|| "comm_r_input"))?;
-
-        // 1. Verify H(Comm_C || comm_r_last) == comm_r
-        {
-            let hash_num = <Tree::Hasher as Hasher>::Function::hash2_circuit(
-                cs.namespace(|| "H_comm_c_comm_r_last"),
-                &comm_c_num,
-                &comm_r_last_num,
-            )?;
-
-            // Check actual equality
-            constraint::equal(
-                cs,
-                || "enforce_comm_c_comm_r_last_hash_comm_r",
-                &comm_r_num,
-                &hash_num,
-            );
-        } 
-
-        let len = leafs.len();
-        let mut gen_cs = cs.make_vector_copy(len)?;
-        let unit = cs.make_copy()?;
-        // 2. Verify Inclusion Paths
-        leafs.into_par_iter().zip(paths.into_par_iter()
-        .zip(gen_cs.par_iter_mut())).enumerate()
-        .for_each( | (i, (leaf, (path, other_cs))) | {
-            PoRCircuit::<Tree>::synthesize(
-                other_cs.namespace(|| format!("challenge_inclusion_{}", i)),
-                Root::Val(*leaf),
-                path.clone(),
-                Root::from_allocated::<CS>(comm_r_last_num.clone()),
-                true,
-            ).unwrap();
-        });
-        for other_cs in gen_cs {
-            cs.part_aggregate_element(other_cs, &unit);
+        else {
+            let mut gen_cs = cs.make_vector_copy(len)?;
+            let unit = cs.make_copy()?;
+            leafs.into_par_iter().zip(paths.into_par_iter()
+            .zip(gen_cs.par_iter_mut())).enumerate()
+            .for_each( | (i, (leaf, (path, other_cs))) | {
+                PoRCircuit::<Tree>::synthesize(
+                    other_cs.namespace(|| format!("challenge_inclusion_{}", i)),
+                    Root::Val(*leaf),
+                    path.clone(),
+                    Root::from_allocated::<CS>(comm_r_last_num.clone()),
+                    true,
+                ).unwrap();
+            });
+            for other_cs in gen_cs {
+                cs.part_aggregate_element(other_cs, &unit);
+            }
         }
 
         Ok(())
